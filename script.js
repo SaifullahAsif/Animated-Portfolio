b4e146c7f3d07cb21df258ce9b1b0538aac3f1c5
SaifullahAsif <saifullahasif9000@gmail.com>
2024-01-26 02:12:49 +0500

    Finalizing the Project

--- script.js
+++ script.js
@@ -2,3 +2,40 @@ Shery.mouseFollower();
 
 Shery.makeMagnet(".magnet");
 
+Shery.hoverWithMediaCircle(".videohover", {
+    videos: ["./public/videos/Blue and White Illustrative Modern Science and Technology Education Video.mp4"],
+  });
+  
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".fleftelem", {
+  scrollTrigger: {
+    trigger: "#FeaturedImages",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftelem");
+Shery.imageEffect(".images", {
+  style: 5,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    sections.forEach(function (section, index) {
+      ScrollTrigger.create({
+        trigger: section,
+        start: "top top",
+        scrub: 1,
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
+});
